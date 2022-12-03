fb2f9a6b1fe159bd74d863158aeaf53d3f1a7290
king-open <kingwang.0624@gmail.com>
2022-12-03 22:05:38 +0800

    Money 页面布局 以及 修改了 meta:vp

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,107 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-warpper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +113,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
