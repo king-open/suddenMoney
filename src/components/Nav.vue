317e96b7086c6875b6fb74e4679bd25112162497
king-open <kingwang.0624@gmail.com>
2022-12-04 20:11:53 +0800

    HTML 和 CSS 写好了

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -21,8 +21,8 @@
 @import "~@/assets/style/helper.scss";
 nav {
     display: flex;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
     flex-direction: row;
+    @extend %outerShadow;
     font-size: 12px;
     > .item {
       padding: 2px 0;
