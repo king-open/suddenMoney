55bc42cf81b108822aabcd0880fc77e6603308e5
king-open <kingwang.0624@gmail.com>
2022-12-05 20:57:01 +0800

    封装 tagList 的增删改查

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,7 +20,6 @@ import NumberPad from '@/components/Money/NumberPad.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component,Watch} from 'vue-property-decorator';
   import recordListModel from '@/models/recordListModel';
-  import tagListModel from '@/models/tagListModel';
 
   const recordList = recordListModel.fetch();
   
