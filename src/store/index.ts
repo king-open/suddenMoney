3bda24c59f51ef91aa60e28ad2aca0699de5abb1
king-open <kingwang.0624@gmail.com>
2022-12-06 15:30:19 +0800

    修复刷新变成 404 的 bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,24 +2,55 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import router from '@/router';
 
-Vue.use(Vuex); // 把 store 绑到 Vue.prototype.$store = store
+Vue.use(Vuex);
 
 type RootState = {
   recordList: RecordItem[],
   tagList: Tag[],
   currentTag?: Tag
 }
-
 const store = new Vuex.Store({
-  state: { // data
+  state: {
     recordList: [],
     tagList: [],
     currentTag: undefined
-  }as RootState,
+  } as RootState,
   mutations: {
     setCurrentTag(state, id: string) {
       state.currentTag = state.tagList.filter(t => t.id === id)[0];
+    },
+    updateTag(state, payload: { id: string, name: string }) {
+      const {id, name} = payload;
+      const idList = state.tagList.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert('标签名重复了');
+        } else {
+          const tag = state.tagList.filter(item => item.id === id)[0];
+          tag.name = name;
+          store.commit('saveTags');
+        }
+      }
+    },
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit('saveTags');
+        router.back();
+      } else {
+        window.alert('删除失败');
+      }
+
     },
     fetchRecords(state) {
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
@@ -28,7 +59,7 @@ const store = new Vuex.Store({
       const record2: RecordItem = clone(record);
       record2.createdAt = new Date();
       state.recordList.push(record2);
-      store.commit('saveRecords')
+      store.commit('saveRecords');
     },
     saveRecords(state) {
       window.localStorage.setItem('recordList',
@@ -49,8 +80,8 @@ const store = new Vuex.Store({
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
-    }
+    },
   }
-  })
+});
 
 export default store;
