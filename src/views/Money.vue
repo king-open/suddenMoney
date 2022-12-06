64a12a6a960122d7a14f500587a4cbd9d8ca2a6e
king-open <kingwang.0624@gmail.com>
2022-12-06 21:31:10 +0800

    删除 Types

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,7 +15,6 @@
 <script lang="ts">
 import Vue from 'vue'
 import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
@@ -25,7 +24,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 
 
   @Component({
-    components: {Tabs,Tags, FormItem, Types, NumberPad},
+    components: {Tabs,Tags, FormItem, NumberPad},
   })
   export default class Money extends Vue {
     get recordList() {
